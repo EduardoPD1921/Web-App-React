bde66be6e182906cbd4f5f1c2347d60b5e4f8434
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-08-10 14:38:54 -0300

    Main implemented

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,9 +3,12 @@ import './Button.css'
 import axios from 'axios'
 
 import Nav from './templates/Nav'
+import Main from './templates/Main'
 
 const initialState = {
-    notes: []
+    notes: [],
+    currentContent: '',
+    defaultNoteName: 'New note'
 }
 
 const baseUrl = 'http://localhost:3001'
@@ -27,11 +30,17 @@ export default class Button extends Component {
         )
     }
 
+    renderNoteContent(e) {
+        e.preventDefault()
+        const noteContent = this.state.notes[e.target.id].content;
+        this.setState({currentContent: noteContent})
+    }
+
     renderNotes() {
         return (
             this.state.notes.map(note => {
                 return (
-                    <a href="/" key={note.id}>
+                    <a href="/" className="navItem" key={note.id} {...note} onClick={e => this.renderNoteContent(e)}>
                         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-caret-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M6 12.796L11.481 8 6 3.204v9.592zm.659.753l5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
                         </svg>
@@ -44,10 +53,17 @@ export default class Button extends Component {
 
     render() {
         return (
-            <Nav>
-                {this.renderNotes()}
-                {this.renderButton()}
-            </Nav>
+            <React.Fragment>
+                <Nav>
+                    {this.renderNotes()}
+                    {this.renderButton()}
+                </Nav>
+                <Main>
+                    <textarea rows="35" cols="60" defaultValue={this.state.currentContent} onChange={e => console.log('teste')}>
+                    </textarea>
+                    {console.log(this.state.currentContent)}
+                </Main>
+            </React.Fragment>
         )
     }
 }
